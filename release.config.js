c371b788a199d57bd2e58f745ee65b04e1e2f642
luis.pflamminger@telekom.de <luis.pflamminger@telekom.de>
2025-07-15 17:57:02 +0200

    ci: add semantic release

---
+++ release.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  branches: ['main'],
+  tagFormat: '${version}',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: "awk -i inplace '{ gsub(/^version:.*/, \"version: ${nextRelease.version}\") }; { print }' Chart.yaml"
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['Chart.yaml', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    '@semantic-release/github'
+  ]
+};
